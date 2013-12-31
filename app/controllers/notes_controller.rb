24d2ce9dd6314fb6f96560f2870563356a29f394
Ananta Pandey <pandananta@gmail.com>
2013-12-31 03:53:23 -0500

    linking is accounted for in suggestions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,8 +13,18 @@ class NotesController < ApplicationController
 
   def show
     @note = @@neo.execute_query("MATCH (n:Note) WHERE n.uid=\"#{params[:id]}\" RETURN n")["data"][0][0]["data"]
-    @suggested_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:contains]->friend-[:comprises]->friend_of_friend WHERE NOT(friend_of_friend.uid = n.uid) AND NOT(n-[:linked]->friend_of_friend)  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid, COUNT(*) ORDER BY COUNT(*) DESC")["data"]
+    # @suggested_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:contains]->friend-[:comprises]->friend_of_friend WHERE NOT(friend_of_friend.uid = n.uid) AND NOT(n-[:linked]->friend_of_friend)  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid, COUNT(*) ORDER BY COUNT(*) DESC")["data"]
     @linked_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:linked]->friend RETURN  friend.prompt, friend.uid")["data"]
+    
+    sug_query = "START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:contains]->friend-[:comprises]->friend_of_friend WHERE NOT(friend_of_friend.uid = n.uid) AND NOT(n-[:linked]->friend_of_friend)  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid, COUNT(*) ORDER BY COUNT(*) DESC"
+    @linked_notes.each do |n|
+      sug_query+=" UNION START n = node(*) WHERE n.uid = \"#{n[1]}\"  MATCH n-[:contains]->friend-[:comprises]->friend_of_friend  WHERE NOT(friend_of_friend.uid = \"#{n[1]}\") AND NOT(friend_of_friend.uid = \"#{params[:id]}\")  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid, COUNT(*) ORDER BY COUNT(*) DESC"
+    end
+
+    @suggested_notes = @@neo.execute_query(sug_query)["data"]
+    puts sug_query
+
+
     respond_to do |format|
       format.html # index.html.erb
     end
