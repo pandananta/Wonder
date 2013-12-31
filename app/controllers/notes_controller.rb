d73f19aa3cd1fe549bbd32ab231bc3c240aff515
Ananta Pandey <pandananta@gmail.com>
2013-12-31 00:23:27 -0500

    suggestsions sorted by frequency of common words, created_at property added to notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,7 +13,7 @@ class NotesController < ApplicationController
 
   def show
     @note = @@neo.execute_query("MATCH (n:Note) WHERE n.uid=\"#{params[:id]}\" RETURN n")["data"][0][0]["data"]
-    @suggested_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:contains]->friend-[:comprises]->friend_of_friend WHERE NOT(friend_of_friend.uid = n.uid) AND NOT(n-[:linked]->friend_of_friend)  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid")["data"]
+    @suggested_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:contains]->friend-[:comprises]->friend_of_friend WHERE NOT(friend_of_friend.uid = n.uid) AND NOT(n-[:linked]->friend_of_friend)  RETURN collect(friend.str), friend_of_friend.prompt, friend_of_friend.uid, COUNT(*) ORDER BY COUNT(*) DESC")["data"]
     @linked_notes = @@neo.execute_query("START n = node(*) WHERE n.uid = \"#{params[:id]}\" MATCH n-[:linked]->friend RETURN  friend.prompt, friend.uid")["data"]
     respond_to do |format|
       format.html # index.html.erb
